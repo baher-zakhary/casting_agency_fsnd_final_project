aeac058ec71bf364317bfcdda5b03542a76df54f
Baher Wael <bwy@crossworkers.com>
2020-08-12 15:43:09 +0200

    Database setup

--- models.py
+++ models.py
@@ -1,1 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
 
+username = 'postgres'
+password = 0000
+database_name = 'castingAgencyDB'
+database_path = "postgres://{}:{}@{}/{}".format(username, password,
+                                                "localhost:5432",
+                                                database_name)
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
